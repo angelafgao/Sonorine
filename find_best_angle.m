c860b607bf1596cc92a1f0164e9fd4cb5e0e0fb8
Angela Gao <angelagao28@gmail.com>
2017-06-13 14:49:48 -0400

    Updated for general width and height

--- find_best_angle.m
+++ find_best_angle.m
@@ -1,16 +1,18 @@
-range = 500;
-l = zeros(range,1);
-for i = 1:range
-    x = (range/2-i)/10;
-    w = 37/2*(abs(sind(x)) + abs(cosd(x))); 
-    h = 25/2*(abs(sind(x)) + abs(cosd(x)));
-    a = atand(w/h);
-    b = atand((h-2)/w);
-    new_angle = 90-a-b;
-    l(i) = x- (90-a-b);
+function angle = find_best_angle(w, h)
+    range = 500;
+    l = zeros(range,1);
+    for i = 1:range
+        x = (range/2-i)/10;
+        w_rot = w/2*(abs(sind(x)) + abs(cosd(x))); 
+        h_rot = h/2*(abs(sind(x)) + abs(cosd(x)));
+        a = atand(w_rot/h_rot);
+        b = atand((h_rot-2)/w_rot);
+        new_angle = 90-a-b;
+        l(i) = x - new_angle;
+    end
+    l = l-1;
+    l = abs(l);
+    minimum = min(l);
+    i = find(minimum==l);
+    angle = (range/2-i)/10;
 end
-l = l-1;
-l = abs(l);
-minimum = min(l);
-i = find(minimum==l)
-angle = (range/2-i)/10

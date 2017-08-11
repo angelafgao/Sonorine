e7a34df172f771e7fe12862a114aa1e4971a0e0b
HarshY <hrsh.ya@gmail.com>
2017-08-11 11:40:44 -0400

    get alignment files

--- get_aligned_dims.m
+++ get_aligned_dims.m
@@ -3,22 +3,24 @@
 % column 3 is image180
 % column 4 is image270
 
-function [alignment, w_alinged, h_aligned] = get_aligned_dims(w, h, trans_info)
-    alignment = zeros(4, 2);
-    min_x = abs(min(trans_info(:, 1)));
-    min_y = abs(min(trans_info(:, 2)));
-    alignment(1, 1) = min_x;
-    alignment(1, 2) = min_y; 
-    for i = 1:3
+function [align, w_alinged, h_aligned] = get_aligned_dims(w, h, trans_info)
+    align = zeros(4);
+    align(3, 1) = w;
+    align(4, 1) = h;
+    for i = 1:4
         % determines x' values
-        alignment(i + 1, 1) = trans_info(i+1, 1) + min_x;
+        align(1, i) = max(0, trans_info(1, i));
         % determines y' values
-        alignment(i + 1, 2) = trans_info(i+1, 2) + min_y;  
+        align(2, i) = max(0, trans_info(2, i));    
+        % determines w' values
+        align(3, i) = w - abs(trans_info(1, i));
+        % determines h' values
+        align(4, i) = h - abs(trans_info(2, i));
     end
     
     % finds the new w and h values for all images
-    w_alinged = w - max(alignment(:, 1));
-    h_aligned = h - max(alignment(:, 2));
+    w_alinged = min(align(3, :));
+    h_aligned = min(align(4, :));
     % saves align as just the start x' and y' values
-    alignment = alignment + 1;
+    align = align(1:2, :) + 1;
 end
--- get_alignment.m
+++ get_alignment.m
@@ -1,36 +1,40 @@
 % set paths 
-source = '../Processed/Cropped/';
-dest = '../Processed/Cropped_aligned_fixed/';
+source = '../../Processed/Roughly_aligned/';
+dest = '../../Processed/Aligned/';
 folder = 'Sonorine_%03d/sonorine_%03d_%03d.tiff';
 source_path = strcat(source, folder);
 dest_path = strcat(dest, folder);
-w = 9600;
-h = 6500;
 
 % extract rotation and translation data for Down1 matrix
-rot_trans_struct = load('translationsMatrix.mat');
-rot_trans = rot_trans_struct.Translations;
+rot_trans_struct = load('../TranslationMatrices/finalTranslationMatrixStretched1.mat');
+rot_trans = rot_trans_struct.FinalMatrix;
 
-trans_info= zeros(4, 2); % load translation and rotation data
-temp_images = zeros(h, w, 4); % store temporarily translated images
+%trans_info= zeros(3, 2); % load translation and rotation data
+%temp_images = zeros(h, w, 4); % store temporarily translated images
 
 % iterate over all of the sonorines
 % rotate and crop all images for each sonorine so that they are 
 % all the same size and same alignment    
-for i = 3:3
-
+for i = 1:5
     % get reference image 000 and save to temp_images
-    temp_images(:, :, 1) = im2double(imread(sprintf(source_path, i, i, 0)));
+    temp_pic_000 = imread(sprintf(source_path, i, i, 0));
+    w = size(temp_pic_000, 2);
+    h = size(temp_pic_000, 1);
+    trans_info= zeros(2, 4); % load translation and rotation data
+    temp_images = zeros(h, w, 4); % store temporarily translated images
     
+    temp_images(:, :, 1) = temp_pic_000;
+    trans_info(1, 1) = 0;
+    trans_info(2, 1) = 0;
     % rotate 0, 90, 180, 270 images
     for j = 1:3
         im = imread(sprintf(source_path, i, i, 90*j));
         % save translation data
-        trans_info(j + 1, 1) = rot_trans((i-1)*3+j, 1);
-        trans_info(j + 1, 2) = rot_trans((i-1)*3+j, 2);
+        trans_info(1, j+1) = rot_trans((i-1)*3+j, 1);
+        trans_info(2, j+1) = rot_trans((i-1)*3+j, 2);
         % rotate and save image into temp_images
         rot = rot_trans((i-1)*3+j, 3);
-        im_rot = imrotate(im, rot, 'bicubic');
+        im_rot = imrotate(im, rot);
         temp_images(:, :, j+1) = cropRotate(im_rot, w, h);
     end
     
@@ -39,8 +43,9 @@ for i = 3:3
     
     % write aligned images into the final file
     for rot = 1:4
-        imwrite(aligned_images(:, :, rot), ...
+        bit16_im = uint16(aligned_images(:, :, rot));
+        imwrite(bit16_im, ...
             sprintf(dest_path, i, i, (rot-1)*90));
     end
-    fprintf('done with %d \n', i);
+    fprintf('Done with sonorine %d\n', i);
 end
